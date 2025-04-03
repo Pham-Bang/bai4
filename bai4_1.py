141ab66e50629ce4ee6fdc21172645660171ffb9
Pham-Bang <pbang14122005@gmail.com>
2025-04-03 15:58:09 +0700

    Add files via upload

---
+++ bai4_1.py
@@ -0,0 +1,5 @@
+S = input('nhap chuoi:')
+for ch in S:
+    print(ch)
+print("sinh vien: Phạm Quang BàngBàng")
+print("Mssv: 2357520207100077")
---
+++ bai4_2.py
@@ -0,0 +1,6 @@
+S = input('Nhap chuoi: ')
+for ch in S:
+    if ch not in (' ', '\t'):
+        print(ch)
+print("sinh vien: Phạm Quang Bàng")
+print("Mssv: 2357520207100077")
